0ecce572c3b6448cd324851c8f252b91bfac382d
Lance A. Brown <lance@bearcircle.net>
2015-09-26 00:12:42 -0400

    Initial commit

---
+++ ConsoleApplication1/Config.cs
@@ -0,0 +1,20 @@
+﻿
+public class Config
+{
+    public Settings settings { get; set; }
+    public Image[] images { get; set; }
+}
+
+public class Settings
+{
+    public string server { get; set; }
+    public string path { get; set; }
+}
+
+public class Image
+{
+    public string id { get; set; }
+    public string operatingsystem { get; set; }
+    public int major { get; set; }
+    public int minor { get; set; }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config;
+
+            using (StreamReader file = File.OpenText(@"test.json"))
+            {
+                JsonSerializer ser = new JsonSerializer();
+                config = (Config)ser.Deserialize(file, typeof(Config));
+            }
+
+            Console.WriteLine("server is " + config.settings.server);
+            Console.ReadLine();
+
+        }
+    }
+}
